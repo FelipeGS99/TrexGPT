f2c0c0b61470727916c7657d75c20a208df88205
Felipe Silva <felipe.gou1999@hotmail.com>
2025-03-13 04:49:55 -0300

    teste webhook 2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-print('Testando')
+print('Testandoo')
 
 openai.api_key = os.getenv('openai_key')
 bot = telebot.TeleBot(os.getenv('telegram_key'))
